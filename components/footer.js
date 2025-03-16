deb233c1a4731d25e7900cd97538d8512b8351c3
Hudsonvoss <Hudson.voss51@buc.blinn.EDU>
2025-03-16 08:30:43 -0500

    push

--- components/footer.js
+++ components/footer.js
@@ -1,16 +1,30 @@
-class Footer extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
+<!-- Begin Font Awesome Row -->
+<div class="container-fluid text-center blinn-fa-container">
+  <div class="row">
+    <div class="col col-12 col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2"><i class="fa-solid fa-dumbbell blinn-fa-icon"></i></div>
+    <div class="col col-12 col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2"><i class="fa-solid fa-face-smile blinn-fa-icon"></i></div>
+    <div class="col col-12 col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2"><i class="fa-solid fa-stopwatch-20 blinn-fa-icon"></i></div>
+    <div class="col col-12 col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2"><i class="fa-solid fa-person-running blinn-fa-icon"></i></div>
+    <div class="col col-12 col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2"><i class="fa-brands fa-pushed blinn-fa-icon"></i></div>
+    <div class="col col-12 col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2"><i class="fa-solid fa-hands-praying blinn-fa-icon"></i></div>
+  </div>
+</div>
+<!-- End Font Awesome Row-->
+<div class="container">
+  <div class="row">
+    <div class="col-12 paragraph-one">
+      <p style="color: blue; background-color: pink; font-family: Verdana, Geneva, Tahoma, sans-serif; padding: 20 px">
+        <img src="images /square/Paragraph-one.jpg" class="paragraph-one-img" alt="Member of the gym" />
 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui exercitationem est obcaecati neque tempore ea, pariatur eius. Veniam, velit doloribus? Error nostrum voluptatum omnis? Officiis minus, odio, aliquam nisi maxime nulla sequi reprehenderit pariatur ratione architecto nemo. Itaque repudiandae laboriosam porro maxime, aspernatur culpa aliquid deserunt nihil enim a ratione nisi modi perspiciatis sit, delectus, tempore tenetur voluptas. Veritatis repellat eos ad fuga natus autem deserunt praesentium ex nesciunt distinctio. Nihil, illum fugit? Placeat illo cupiditate atque adipisci doloribus iure porro deleniti. Minus ipsam optio quo unde sunt. Harum assumenda in reiciendis libero iusto, veritatis fugiat commodi illum aut obcaecati voluptatibus magni placeat eius. Ipsum optio perspiciatis nesciunt dicta nostrum laboriosam laudantium veniam, enim consectetur quae aliquam itaque earum incidunt magni accusantium numquam facere sed. Quasi explicabo temporibus animi sunt,
+        amet quam. Et possimus recusandae officiis ducimus minima voluptas obcaecati molestias porro odit quis aut corporis voluptate reiciendis error sit, autem eligendi hic impedit, rem voluptatibus numquam tempora quos! Fugit debitis quis numquam porro, unde soluta nesciunt dolor expedita? A hic suscipit beatae atque reiciendis libero, odio nisi, cumque asperiores distinctio necessitatibus excepturi voluptas. Sed porro aliquam obcaecati libero ullam nihil numquam eveniet nesciunt. Ipsam ea labore fuga nostrum neque?
+      </p>
+    </div>
+  </div>
+</div>
+</main>
 
-<!-- your common html goes here -->
-
-            `;
-    }
-  }
-  
-  customElements.define('footer-component', Footer);
+<footer>Some footer content</footer>
+<section>
+<!-- Bootstrap CDN JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
